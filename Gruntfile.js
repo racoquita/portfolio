529bf045a0d5bacab57cd0c7367849d7a4ead927
Richard Stovall <rstovall@me.com>
2014-05-25 22:56:02 -0400

    adding the v2 project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,86 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    handlebars: {
-      dev: {
-        files:{
-          'js/templates.js': 'hbs/**/*.hbs'
-        },
-        options:{
-          namespace: 'raco', 
-          partialRegex: /^_/,
-          processName: function (filename) {
-            return filename.replace('hbs/', '').replace('.hbs', '')
-          } 
-        } 
-      }
-    },
-    watch:{
-      hbs:{
-        files: 'hbs/**/*.hbs',
-        tasks: ['handlebars:dev']
-      }
-    }
+	grunt.task.loadNpmTasks('grunt-contrib-handlebars');
+	grunt.task.loadNpmTasks('grunt-contrib-compass');
+	grunt.task.loadNpmTasks('grunt-contrib-connect');
+	grunt.task.loadNpmTasks('grunt-contrib-watch');
 
-    
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		handlebars: {
+			build: {
+				options: {
+					namespace: 'raco',
+					processName: function(filename) {
+						return filename.replace('hbs/', '').replace('.hbs', '');
+					}
+				},
+				files: {
+					'js/templates.js': 'hbs/**/*.hbs'
+				}
+			}
+		},
+		compass: {
+			build: {
+				options: {
+					sassDir: 'scss',
+					cssDir: 'css',
+					outputStyle: 'expanded',
+					force: true,
+					noLineComments: false
+				}
+			},
+			minify: {
+				options: {
+					sassDir: 'scss',
+					cssDir: 'css',
+					outputStyle: 'compressed',
+					force: true,
+					noLineComments: true
+				}
+			}
+		},
+		connect: {
+			build: {
+				options: {
+					hostname: '',
+					port: 8002,
+					base: '',
+					open: true
+				}
+			}
+		},
+		watch: {
+			scss: {
+				files: 'scss/**/*.scss',
+				tasks: ['compass:build'],
+				options: {
+					livereload: true
+				}
+			},
+			hbs: {
+				files: 'hbs/**/*.hbs',
+				tasks: ['handlebars:build'],
+				options: {
+					livereload: true
+				}
+			},
+			js: {
+				files: 'js/**/*.js',
+				tasks: [],
+				options: {
+					livereload: true
+				}
+			},
+			once: {
+				files: 'index.html',
+				tasks: [],
+				options: {
+					livereload: true
+				}
+			}
+		}
+	});
 
-  // Default task(s).
-  grunt.registerTask('default', ['handlebars:dev', 'watch']);
-
+/* primary task */
+return grunt.registerTask('default', ['handlebars:build', 'compass:build', 'compass:minify', 'connect:build', 'watch']);
 };
--- js/main.js
+++ js/main.js
@@ -1,282 +1,103 @@
-var about; 
-var portfolio;
-var faq;
-var contact;
-var id;
-var view;
-var inViewSection;
-var bg;
-var galleryItem;
-var closeLink;
-var navLink;
-var header;
-var currentSection;
-var activeNav;
-var to;
-var interval;
-var inview = false;
-var temp;
-var scrolling=false;
-var scrollBox;
-var info;
-var selLi;
-var port;
-var container;
+var scroller;
+var g_scroller;
+var appended = false;
+var startPos = 0;
+var prevPos = 0;
+var fromClick = false;
 
-function getInnerWidth(){
+$(document).ready(function(){
+	initPackery();
+	initScroller();
+	initGalleryScroll();
+	handleNav();
+});
 
-	return (window.innerWidth) ? window.innerWidth : ((document.documentElement.clientWidth) ? document.documentElement.clientWidth : document.body.clientWidth);
+var initPackery = function () {	 
+	$('#gallery').packery({
+		columnWidth: '.grid-sizer',
+		isHorizontal: true,
+		gutter:10,
+		itemSelector: '.item'
+	});
 }
-function getInnerHeight(){
 
-	return (window.innerHeight) ? window.innerHeight : ((document.documentElement.clientHeight) ? document.documentElement.clientHeight : document.body.clientHeight);
-}
-var getCurrentSection = function () {
-	
-	bg.each(function(index, el){
-		
-		var el = $(el);
-		if (el.visible(true)) {
-			
-		    inViewSection = el.attr('id');
-		} 
+function initScroller() {
+	$('#scroll, ul.wrapper li.bg').height($(window).height());
+	$('.wrapper').height($(window).height() * 3);
+
+	scroller = new IScroll('#scroll', {
+		scrollX: false,
+		scrollY: true,
+		probeType: 3,
+		mouseWheel: true
 	});
 
-	return inViewSection
-}
-var clearTimeouts = function() {
-	clearTimeout(to[0]);
- 	clearTimeout(to[1]);
-}
-var animateTo = function(currSection){
-	//scrolling = true;
+	scroller.on('scroll', function() {
+		checkDir(this.y);
 
-	updateNav($(currSection).attr('id'));
+		if($('#portfolio').position().top <= $('header:not(.clone)').height()) {
+			if(!appended) {
+				var clone = $('#about').find('header').clone();
+					clone.addClass('clone');
 
-	var tl = new TimelineMax({
-		onComplete: function() {
-			console.log('done')
-			scrolling = false;
+				$('#about').find('header').hide();
+				$('body').append(clone);
+				
+				appended = true;
+				handleNav();
+			}
+		} else {
+			$('header.clone').remove();
+			$('#about').find('header').show();
+			appended = false;
 		}
 	});
 
-	tl.to( window, 1, { scrollTo:{y:currSection.offset().top}, ease:Power2.easeOut});
-
-}
-var updateNav = function(section){
-
-	activeNav = $('nav a[data-scroll="'+section+'"]');
-	if(! activeNav.hasClass('active')) {
-		$('.active').removeClass('active');
-		activeNav.addClass('active');
-	}
-	stickyNav();
-}
-var checkWindowTop = function(){
-	return $(window).scrollTop();
-}
-var stickyNav = function() {
-	var scrollTop =  $(window).scrollTop();
-	if (scrollTop >= $('nav').offset().top && ! header.hasClass('stick')) {   
-	    header.addClass('stick');  
-	   
-	}else if(scrollTop <  625 ){
-		
-		header.removeClass('stick');
-	}
-	
-}
-var handleScrolling = function () {
-	//if(scrolling) return;
-	bg.each(function(i, el) {
-	    var el = $(el).children();
-	   
-	    if (el.visible(false)) {
-	      el.addClass("come-in"); 
-	    }
+	scroller.on('scrollStart', function(){
+		startPos = this.y;
+		checkDir(this.y);
 	});
 }
-var initPackery = function () {
-	console.log('initializing packery')
-	 
-	container.packery({
-	  columnWidth: '.grid-sizer',
-	  gutter:10,
-	  itemSelector: '.item'
-	});
-}
-var init = function(){
-	$.getJSON('json/data.json', function(res){
-		port = res;
-	});
 
-	Modernizr.load({  
-	    test: Modernizr.touch,  
-	    yep : 'touchscroll.js',  
-	});
-	about  = $('#about');
-  	portfolio = $('#portfolio');
-  	faq = $('#faq');
-  	contact = $('#contact');
-  	bg = $('.bg');
-  	galleryItem = $('#gallery li');
-  	closeLink = $('.close');
-  	navLink = $('nav a');
-  	header = $('header');
-  	scrollBox = $('#wrapper');
-  	container = $('#gallery');
-  	var to = [];
-  	
-	$('.part2').fadeIn('slow').delay(100).fadeOut('slow');
-	$('.scroll-signifier').toggleClass('hidden').addClass('come-in');
-	$('#sm').toggleClass('hidden').addClass('come-in');
-	
-  	stickyNav();
+function handleNav() {
+	$('header a').on('click', function(e){
+		e.preventDefault();
+		fromClick = true;
 
-  	setTimeout(function(){
-  		$('.scroll-signifier').removeClass('come-in').addClass('animated bounce');
-  	}, 1000);
-  	
-}
-var test = function() {
-	alert('done')
-	
-}
-var resetNavPosition = function() {
-	header.css({top: getInnerHeight() - header.height() +'px'})
-}
-var setItemGalleryHeight = function() {
-	// if(getInnerHeight() < 500){
-	// 	galleryItem.css({height: getInnerHeight() / 3+'px'})
-	// }
-	
-}
-var grabAllfromSelCat = function(worksArr, cat){
-		var works = []
-		$.each(worksArr, function(i, work) {
-			
-			if(work.category == cat){
-				console.log(work.category);
-			}
+		var target = $(e.currentTarget).data('scroll');
 
-		});
-}
-var itemClickHandler = function (e) {
-	
-	selLi = $(e.target).closest('li');
-		var selected = selLi.find('.contain').data('name');
-		var categorySelected = selLi.find('.contain').data('category')
-		var data = port[selected];
-		
-		//grabAllfromSelCat(data.category)
-		grabAllfromSelCat(port, data.category);
-		//var clone = selLi.find('.contain').clone();
-		var clone = $('<div class="contain"></div>');
-		
-		clone.addClass('clone').css({
-			top: selLi.position().top,
-			left:selLi.position().left,
-			width:selLi.width(),
-			height:selLi.height()
-		});
-		
-		$(portfolio).append(clone);
-		$(clone).html(raco.selectedWork(data));
-		// $('#imgFull').jslide();
-		
-		setTimeout(function() {
-			$(clone).addClass('full');
+		if(typeof scroller != 'undefined') {
+			scroller.scrollToElement('#' + target, 500);
+
+			$('header a').removeClass('active');
+			$('header a[data-scroll="'+ target +'"]').addClass('active');
 
 			setTimeout(function(){
-				$(clone).find('img').css('opacity', 1);
-				$(clone).find('.expanded-info').css('opacity', 1);
-				$(clone).find('dl').css('opacity', 1);
-				$(clone).children().css('opacity', 1);
-			}, 10);
-		}, 10);
-		
-		closeLink.show();
-		animateTo(portfolio);
+				fromClick = false;
+			}, 500);
+		}
+	});
 }
 
-$(document).ready(function() {
+function checkDir(y) {
+	var dir = prevPos - y;
+		prevPos = y;
 
-	init();
-	//initMasonry();
-	initPackery();
-	navLink.on('click', function (e){
-		id = $(e.currentTarget).data('scroll');
-		navLink.removeClass('active');
-		$(e.currentTarget).addClass('active');
-		animateTo($('#'+id));
-		return false;
-
-	});
-	galleryItem.on('mouseenter', function(e) {
-		
-		var img = $(e.target).closest('li').find('img');
-		info = $(e.target).closest('li').find('.info');
-		info.addClass('hover');
-		var hover = $(e.target).closest('li').find('.info.hover');
-		TweenLite.to(hover, 1, {top:0, left:0});
-		return false;
-
-	}).on('mouseleave', function (evt) {
-		
-		info = $(evt.target).closest('li').find('.info');
-		info.removeClass('hover');
-		
-		TweenLite.to(info, 1, { top:300, left:0});
-		return false	
-	});
-	if(Modernizr.touch){
-		galleryItem.off('mouseenter');
+	if(appended && !fromClick) {
+		if(dir > 0) $('.clone').addClass('hide');
+		if(dir < 0) $('.clone').removeClass('hide');
 	}
+}
 
-	galleryItem.on('click', function (e){
+function initGalleryScroll() {
+	g_scroller = new IScroll('#g-scroll', {
+		scrollX: true,
+		scrollY: false,
+		probeType: 3,
+		mouseWheel: false
+	});
 
-		itemClickHandler(e);
-		return false;
+	g_scroller.on('beforeScrollStart', function(){
+		console.log(this);
 	});
-	
-	closeLink.on('click', function (e) {
-		$('.clone').remove();
-		$(this).hide();
-		animateTo(portfolio);
-		return false;
-	});
-	$( "form" ).on( "submit", function( event ) {
-		$.ajax({
-		  url: "send_form_email.php",
-		  context: document.body,
-		  success: function (response) {
-		  	console.log(response);
-		  }
-		})
-	 	return false;
-	});
-	$(window).scroll(function() {
-		
-		if($(window).scrollTop() > $('nav').offset().top || $(window).scrollTop() <  getInnerHeight() - header.height() ){
-		
-			stickyNav();
-			handleScrolling();
-			updateNav(getCurrentSection());
-			
-		}else {
-			
-			return
-		}
-
-	});
-	$(window).scrollStopped(function(){
-		
-		scrolling = false;
-		updateNav(getCurrentSection());
-	});
-	$(window).resize(function(argument) {
-		
-		setItemGalleryHeight();
-	});
-	
-});
+}
